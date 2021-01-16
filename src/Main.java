418f0482f5810c6bfaa6069c5f2a780b2a082b2f
Dmitriy Dolmatov <Rabmail@mail.ru>
2021-01-16 16:34:18 +0300

    Edit project

---
+++ src/Main.java
@@ -0,0 +1,12 @@
+public class Main {
+    public static void main(String[] args) {
+
+
+        long ticket = 199_99;
+        long limit = 20;
+        long bonus = ticket / limit /100;
+
+               System.out.println(bonus);
+    }
+}
+
