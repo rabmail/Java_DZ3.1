7148b7c6725231f12bd40ab5281df5845b9d6318
Dmitriy Dolmatov <Rabmail@mail.ru>
2021-01-17 15:54:45 +0300

    Edit Project

--- src/Main.java
+++ src/Main.java
@@ -5,10 +5,10 @@ public class Main {
         long ticket = 199_99;
         long limit = 20;
         //отбрасываем дробную часть
-        long round = ticket / limit/100;
+        long round = ticket / limit / 100;
         // добавляем копейки 00
-        long  bonus = round * 100;
-               System.out.println(bonus);
+        long bonus = round * 100;
+        System.out.println(bonus);
     }
 }
 
